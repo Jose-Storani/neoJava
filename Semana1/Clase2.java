f711ebb2779f78252e3fd181da737b3404827b08
Jose Storani <jdstorani91@gmail.com>
2023-09-28 20:41:25 -0300

    first commit

---
+++ Semana1/Clase2.java
@@ -0,0 +1,10 @@
+package Semana1;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CursoJavaNeoris {
+}
